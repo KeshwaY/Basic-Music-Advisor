2d5eddc145332e2d15d391808c28f9be24aab1f3
Damian Ciepiela <dciepiela@yahoo.com>
2022-01-05 12:28:20 +0100

    feat: JsonConverter from string to JsonObject

---
+++ src/advisor/core/JsonConverter.java
@@ -0,0 +1,12 @@
+package advisor.core;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class JsonConverter {
+
+    public JsonObject convert(String json) {
+        return JsonParser.parseString(json).getAsJsonObject();
+    }
+
+}
---
+++ test/advisor/core/JsonConverterTest.java
@@ -0,0 +1,24 @@
+package advisor.core;
+
+import com.google.gson.JsonObject;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class JsonConverterTest {
+
+    private JsonConverter converter = new JsonConverter();
+
+    @Test
+    void itShouldConvertToJson() {
+        // given
+        String json = "{\"key\":\"value\"}";
+
+        // when
+        JsonObject jsonObject = converter.convert(json);
+
+        // then
+        assertThat(jsonObject.get("key").getAsString())
+                .isEqualTo("value");
+    }
+}
