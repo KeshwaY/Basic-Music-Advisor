1289557560bc2403c7fc7aa82844cd2647d44205
Damian Ciepiela <dciepiela@yahoo.com>
2022-01-20 23:32:12 +0100

    feat: ComponentProvider now can sand api requests

--- src/advisor/core/ComponentProviderImpl.java
+++ src/advisor/core/ComponentProviderImpl.java
@@ -3,34 +3,72 @@ package advisor.core;
 import advisor.core.abstraction.ComponentProvider;
 import advisor.core.abstraction.RequestSender;
 import advisor.core.components.*;
+import advisor.core.spotifyapi.JsonConverter;
+import com.google.gson.JsonObject;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 public class ComponentProviderImpl implements ComponentProvider {
 
     private final RequestSender sender;
+    private final String resourceURI;
+    private final JsonConverter jsonConverter;
+    private final JsonComponentConverter componentConverter;
 
-    public ComponentProviderImpl(RequestSender sender) {
+    public ComponentProviderImpl(RequestSender sender, String resourceURI) {
+        this.jsonConverter = new JsonConverter();
+        this.componentConverter = new JsonComponentConverter();
+        this.resourceURI = resourceURI;
         this.sender = sender;
     }
 
     @Override
-    public List<Album> getNewReleases() {
-        return FakeApiResponses.getNew();
+    public List<Album> getNewReleases(String accessToken) throws IOException, InterruptedException {
+        HttpResponse<String> response = sender.sendGetRequestWithHeaders(
+                URI.create(resourceURI + "/browse/new-releases"),
+                createHeaders(accessToken)
+        );
+        JsonObject jsonObject = jsonConverter.convert(response.body());
+        return componentConverter.jsonToAlbums(jsonObject);
     }
 
     @Override
-    public List<Playlist> getFeatured() {
-        return FakeApiResponses.getFeatured();
+    public List<Playlist> getFeatured(String accessToken) throws IOException, InterruptedException {
+        HttpResponse<String> response = sender.sendGetRequestWithHeaders(
+                URI.create(resourceURI + "/browse/featured-playlists"),
+                createHeaders(accessToken)
+        );
+        JsonObject jsonObject = jsonConverter.convert(response.body());
+        return componentConverter.jsonToPlaylist(jsonObject);
     }
 
     @Override
-    public List<Category> getCategories() {
-        return FakeApiResponses.getCategories();
+    public List<Category> getCategories(String accessToken) throws IOException, InterruptedException {
+        HttpResponse<String> response = sender.sendGetRequestWithHeaders(
+                URI.create(resourceURI + "/browse/categories"),
+                createHeaders(accessToken)
+        );
+        JsonObject jsonObject = jsonConverter.convert(response.body());
+        return componentConverter.jsonToCategories(jsonObject);
     }
 
     @Override
-    public List<CategoryPlaylist> getPlaylistsForCategory(String categoryName) {
-        return FakeApiResponses.getMoodCategory();
+    public List<CategoryPlaylist> getPlaylistsForCategory(String accessToken, String categoryId) throws IOException, InterruptedException {
+        HttpResponse<String> response = sender.sendGetRequestWithHeaders(
+                URI.create(resourceURI + String.format("/browse/categories/%s/playlists", categoryId)),
+                createHeaders(accessToken)
+        );
+        JsonObject jsonObject = jsonConverter.convert(response.body());
+        return componentConverter.jsonToCategoryPlaylist(categoryId, jsonObject);
+    }
+
+    private List<String> createHeaders(String token) {
+        return List.of(
+                "Content-Type", "application/json",
+                "Authorization", "Bearer " + token
+        );
     }
 }
--- src/advisor/core/abstraction/ComponentProvider.java
+++ src/advisor/core/abstraction/ComponentProvider.java
@@ -5,13 +5,14 @@ import advisor.core.components.Category;
 import advisor.core.components.CategoryPlaylist;
 import advisor.core.components.Playlist;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ComponentProvider {
 
-    List<Album> getNewReleases();
-    List<Playlist> getFeatured();
-    List<Category> getCategories();
-    List<CategoryPlaylist> getPlaylistsForCategory(String categoryName);
+    List<Album> getNewReleases(String accessToken) throws IOException, InterruptedException;
+    List<Playlist> getFeatured(String accessToken) throws IOException, InterruptedException;
+    List<Category> getCategories(String accessToken) throws IOException, InterruptedException;
+    List<CategoryPlaylist> getPlaylistsForCategory(String accessToken, String categoryId) throws IOException, InterruptedException;
 
 }
