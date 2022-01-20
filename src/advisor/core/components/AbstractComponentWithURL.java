8569fa9f1e8be3f688978a06c49dfa55f46b3145
Damian Ciepiela <dciepiela@yahoo.com>
2022-01-20 23:27:08 +0100

    feat: Abstraction for component with URL

---
+++ src/advisor/core/components/AbstractComponentWithURL.java
@@ -0,0 +1,13 @@
+package advisor.core.components;
+
+import java.net.URL;
+
+public class AbstractComponentWithURL extends AbstractComponent {
+
+    protected URL url;
+
+    public URL getUrl() {
+        return url;
+    }
+
+}
